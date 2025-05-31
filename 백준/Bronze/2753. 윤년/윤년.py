32271357b1c82139c9bd424d053e102c3777935d
hyxx-su <lulanglangl@gmail.com>
2025-05-31 23:59:12 +0900

    [Bronze V] Title: 윤년, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,6 +1,6 @@
-a = int(input())
+n = int(input())
 
-if a%4==0 and (a%100!=0 or a%400==0):
-    print("1")
+if n % 4 == 0 and (n % 100 != 0 or n % 400 == 0):
+    print(1)
 else:
-    print("0")
+    print(0)
