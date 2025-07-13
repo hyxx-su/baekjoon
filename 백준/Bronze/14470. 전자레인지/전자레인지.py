20df79c7e7a85c90642bab91df91c1eb3d3a7959
hyxx-su <lulanglangl@gmail.com>
2025-07-13 23:59:48 +0900

    [Bronze IV] Title: 전자레인지, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/14470. 전자레인지/전자레인지.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+
+if a < 0:
+    a *= -c
+    b *= e
+    print(a+b+d)
+elif a == 0:
+    print((b-a)*e)
+else:
+    print((b-a)*e)
