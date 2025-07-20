041f39021fa6ebb60cfbd61fe1d165523b4e38ab
hyxx-su <lulanglangl@gmail.com>
2025-07-21 03:09:12 +0900

    [Bronze III] Title: 알람 시계, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,14 +1,14 @@
-h, m = map(int, input().split())
+n, m = map(int, input().split())
 
-if m>=45:
-    m-=45
-    print(h, m)
+if m >= 45:
+    m -= 45
+    print(n, m)
 else:
-    if h!=0:
-        h-=1
-        m+=15
-        print(h, m)
-    elif h==0:
-        h=23
-        m+=15
-        print(h, m)
+    if n != 0:
+        n -= 1
+        m += 15
+        print(n, m)
+    elif n == 0:
+        n = 23
+        m += 15
+        print(n, m)
